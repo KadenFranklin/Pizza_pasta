e8296690b44ba1f8d2f13984f31cbd59dc200eb4
kaden <kadenfranklin@hotmail.com>
2022-04-24 19:36:18 -0500

    reformatted to one file.
    IDK why I do that

--- dect_face.py
+++
@@ -1,7 +0,0 @@
-# C:\Users\Kaden's Laptop\Desktop\#assets
-# or
-# C:\Users\Kaden's Laptop\Documents\GitHub\KadenFranklin.github.io\csci270\Pasta, Pizza, Person\#assets
-
-# want to have some sort of working facial detection program using haarcascade
-
-# https://www.youtube.com/watch?v=LopYA64KmdE
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import os
+import json
+import lzma
 
 # "C:\Users\Kaden's Laptop\Downloads\assets"
+path = r"C:\Users\Kaden's Laptop\Desktop\assets"
 
 
 # Timeline:
@@ -14,9 +18,14 @@
 # want to have a working version of haar-cascade training algorithm or whatever
 # start creating individual modules that detect a specific ingredient
 
+# by 5/6
+# hopefully all code works long before the time I have to present,
+# and I will have plenty of time to analyse my data
+# and everything will be okay.
+
 # A main algorithm that will look something like this
 
-#
+#       I might have to loop through the files to create these but
 #       list_of_piz = [] (a list of lists(lists are pizza ingredients))
 #       list_of_per = [] (or maybe a dict) entries are (count, img/path )
 #
@@ -47,8 +56,73 @@
 #
 
 
-# by 5/6
-# hopefully all code works long before the time I have to present,
-# and I will have plenty of time to analyse my data
-# and everything will be okay.
+def pizza_pie():
+    deez = 0
+    # to detect different ingredients in an image, if the image is a slice of pizza
 
+
+def dect_face():
+    deez = 1
+    # want to have some sort of working facial detection program using haarcascade
+    # https://www.youtube.com/watch?v=LopYA64KmdE
+
+    # for every image in directory
+    #   if face
+    #   move to subdirectory
+
+
+def not_comment():
+    # this was originally meant to pull comments from the posts,
+    # json files did not contain comments, but do contain likes,
+    # and alt image text which I think will be funny to do sentiment analysis on
+
+    count = 0
+    for file in os.listdir(path):
+        if file.endswith('.xz'):
+            new_file = file
+            new_file = new_file[:-8]
+            new_file = new_file + '.txt'
+            print(new_file)
+            count += 1
+            f = lzma.open(path + '\\' + file)
+            json_bytes = f.read()
+            stri = json_bytes.decode('utf-8')
+            data = json.loads(stri)
+            for x in data.values():
+                print(x)
+                count_2 = 1
+                for y in x.keys():
+                    another_count = 0
+                    print(f"Element #: {count_2}")
+                    print(y)
+
+                    # rn this part is not working as intended, loops through
+                    # every post, then does the first write for some reason
+
+                    while y == 'accessibility_caption':
+                        another_count += 1
+                        for y2 in x.values():
+                            print(f"{another_count}: {y2}")
+                            if y2 is None:
+                                break
+                            if (another_count == count_2) & (y2 is str):
+                                with open(new_file, "a") as f:
+                                    f.write("\n")
+                                    f.write(y2)
+                                    print('successfully performed a write... my code works?!?!?!')
+                                    break
+
+                    while y == 'edge_liked_by':
+                        another_count += 1
+                        for y2 in x.values():
+                            print(f"{another_count}: {y2}")
+                            if y2 is None:
+                                break
+                            if (another_count == count_2) & (y2 is str):
+                                with open(new_file, "a") as f:
+                                    f.write("\n")
+                                    f.write(y2)
+                                    print('successfully performed a write... my code works?!?!?!')
+                                    break
+
+                    count_2 += 1
