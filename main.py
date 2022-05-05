f7690abc57f9a3210f33b46ed811d1f9d261ff2f
kaden <kadenfranklin@hotmail.com>
2022-05-05 13:45:21 -0500

    forgot main lol

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
 import os
 import json
 import lzma
+import shutil
 import argparse
 import cv2 as cv
-
-# To initialize the dataset must use a Library called instaloader.
-# It did not work on my machine, but here is the code to replicate an instagram dataset.
-
-# import instaloader
-# L = instaloader.Instaloader()
-# 	for post in Post.from_username(L.context, ‘eric_thepizzaguy’).get_posts():
-# 		L.download_post(post, target = ‘#assets’)
-
-# "C:\Users\Kaden's Laptop\Downloads\assets"
-path = r"C:\Users\Kaden's Laptop\Desktop\assets"
-
 
 # Timeline:
 # by 4/22
@@ -27,68 +16,28 @@ path = r"C:\Users\Kaden's Laptop\Desktop\assets"
 # want to have a working version of haar-cascade training algorithm or whatever
 # start creating individual modules that detect a specific ingredient
 
-# (These were attainable goals, but I simply got preoccupied with other things)
-# Moving forward:
-# 1 Week Till presentation
+# These were attainable goals, but I simply got preoccupied with other things
 
 # If I get everything else working I will work on not_comment again.
 # dect_face - Checks if an img contains a face and moves it to the People directory
 # get_face_count - Goes through People directory and tries to detect the same face multiple times
 #                   (Want this to return some count of people, or x most common faces)
-# make_pizza(s) - make s subdirectory in Pizza
+# make_pizza(s) - generates the xml file for preset of name 's'
 # pizza_pie -
 
-# Main Algorithm -
+# To initialize the dataset must use a Library called instaloader.
+# It did not work on my machine, but here is the code to replicate an instagram dataset.
 
-# will look something like this
-
-#       I might have to loop through the files to create these but
-#       list_of_piz = [] (a list of lists(lists are pizza ingredients))
-#       list_of_per = [] (or maybe a dict) entries are (count, img/path )
-#
-#       for every file:
-#           -if person:
-#              - if current person is similar enough to some other entry in list_of_per
-#                   count += 1
-#              - else:
-#                   make a new entry in list_of_per
-#           - if !person:
-#              - if pizza
-#                    check what kind of ingredients,
-#                    store in list
-#                    append to list_of_piz
-#              - if !pizza
-#                    not to be rude but kinda irrelevant
-#
-#       find person with the highest count, maybe incorporate likes
-#       into calc somewhere
-#           can also do sentiment analysis on posts including people ig
-#           also do something with most liked post of a person
-#
-#       should make a frequency chart of ingredients over time
-#           use the filenames in some way so that they are displayed
-#           properly in chronological order
-#
-# To create a preset you must create a file beforehand to store the trained cascade classifier
-# Then run make_pizza(s) where s is the file path of images you want to use as positive samples
-# In my case I made.
-#   Pepperoni
-#   Sausage
-#   Mushroom
-#   Ham
-#   Tomato
-#   Olive
-#   Bacon
-#   Pickle
-#   Chicken
-#   Onion
-#   Jalapeño
-#   Macaroni - might give false positives
+# import instaloader
+# L = instaloader.Instaloader()
+# 	for post in Post.from_username(L.context, ‘eric_thepizzaguy’).get_posts():
+# 		L.download_post(post, target = ‘#assets’)
 
 
 def pizza_pie():
-    # i will make this some helper function for make_pizza()
-    #(idk what yet but I'll prob need to just space it out)
+    x = 1
+    # I will make this some helper function for make_pizza()
+    # (idk what yet but I'll prob need to just space it out)
     # to detect different ingredients in an image, if the image is a slice of pizza
 
 
@@ -99,17 +48,42 @@ def make_pizza(s):
 
 
 def dect_face():
-    # https://www.youtube.com/watch?v=LopYA64KmdE
+    # I had to use multiple cascade classifiers to check each image. I was getting a good number of false positives.
+
     face = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
     eye = cv.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
+    alt = cv.CascadeClassifier('haarcascade_frontalface_alt.xml')
+    alt2 = cv.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+    alttre = cv.CascadeClassifier('haarcascade_frontalface_alt_tree.xml')
+
     for file in os.listdir(path):
         if file.endswith('.jpg'):
-            img = cv.imread(file)
+            n_path = path + '\\' + file
+            img = cv.imread(n_path)
             gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+            gray = cv.equalizeHist(gray)
             faces = face.detectMultiScale(gray, 1.1, 4)
-            for (x, y, w, h,) in faces:
-                cv.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
-            new_dir = r"C:\Users\Kaden's Laptop\Desktop\assets\person"
+
+            if faces is not None:
+                for (x, y, w, h) in faces:
+                    faceROI = gray[y:y+h, x:x+w]
+                    eyes = eye.detectMultiScale(faceROI, 1.1)
+                    alts = alt.detectMultiScale(gray, 1.1)
+                    alts2 = alt2.detectMultiScale(gray, 1.1)
+                    altstre = alttre.detectMultiScale(gray, 1.1)
+                    print(f"File: {file} \n face:{len(faces)} \n eye:{eyes} \n alt1:{alts} \n alt2:{alts2} \n alt3:{altstre} \n ")
+                    cv.imshow("Image", img)
+                    cv.waitKey(1)
+                    # An image most likely contains a face if the following classifiers are not none:
+                    # frontal_face and eye_tree_eyeglass
+                    # frontal_face and alt1, alt2, alt3
+                    if len(faces) > 1 and len(eyes) >= 1:
+                        shutil.copyfile(n_path, person + '\\' + file)
+                        print(f"con. 1 copied {file} file. \n")
+                    elif len(faces) >= 1 and (len(alts) >= 1 and len(alts2) >= 1 and len(altstre) >= 1):
+                        shutil.copyfile(n_path, person + '\\' + file)
+                        print(f"con. 2 copied {file} file. \n")
+
 
 
 def not_comment():
@@ -155,3 +129,50 @@ def not_comment():
                                     f.write(val)
                                     print('successfully performed a write... my code works?!?!?!')
                                     break
+
+
+# Main Algorithm
+# Uses code above to loop through images in pizza and person directories.
+# Will attempt to create a frequency count of specific person.
+# checks each image against presets - stores values in a dict where key is filename of img(date), and value is list
+# of ingredients I might have to loop through the files to create these but list_of_piz = []
+# (a list of lists(lists are pizza ingredients))
+# "C:\Users\Kaden's Laptop\Downloads\assets"
+path = r"C:\Users\Kaden's Laptop\Desktop\assets"
+pizza = r"C:\Users\Kaden's Laptop\Desktop\assets\Pizza"
+pasta = r"C:\Users\Kaden's Laptop\Desktop\assets\Pasta"
+person = r"C:\Users\Kaden's Laptop\Desktop\assets\Person"
+dect_face()
+
+# pers_list = []
+#       for peep in person:
+#           if person not in list,
+#               append to list
+#           else:
+#               make a new entry in list_of_per
+#
+#       find person with the highest count, maybe incorporate likes into calc somewhere
+#           can also do sentiment analysis on posts including people ig
+#           also do something with most liked post of a person
+
+#       for pizz in pizza:
+#
+#
+#       should make a frequency chart of ingredients over time
+#           use the filenames in some way so that they are displayed
+#           properly in chronological order
+#
+# To create a preset you must create a file beforehand to store the trained cascade classifier
+# Then run make_pizza(s) where s is the file path of images you want to use as positive samples
+# In my case I made.
+#   'Pepperoni'
+#   'Sausage'
+#   'Mushroom'
+#   'Ham'
+#   'Tomato'
+#   'Olive'
+#   'Bacon'
+#   'Pickle'
+#   'Chicken'
+#   'Onion'
+#   'Jalapeño'
